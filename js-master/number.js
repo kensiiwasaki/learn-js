8d1ce0a423b93b18efe5e31eb69c0a53cc7a9f53
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-18 12:07:35 +0900

    commit

---
+++ js-master/number.js
@@ -0,0 +1,17 @@
+let youtuber ={
+    list: {
+        business: {
+            youtuber: [
+                { name: "しまぶー", age: 20, teachPrograming() {} },
+                { name: "あちゃん", age: 37, teachHistory() {} }
+            ],
+            teach() {}
+        },
+        entertainment: {
+            youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
+            makeSmile() {}
+        }
+    },
+    plan() {},
+    uploadVideo() {}
+};
