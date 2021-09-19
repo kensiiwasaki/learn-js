2cfec3103fc1df27120dc718bde5db2cac18cc02
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-19 14:20:47 +0900

    リファクタリング

--- js-master/number.js
+++ js-master/number.js
@@ -8,15 +8,16 @@ async function getUsers() {
     return users;
 }
 
+function addList(user) {
+    // DOM操作
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list);
+}
+
 async function listUsers() {
     const users = await getUsers();
-
-    // DOM操作
-    users.forEach(function(user) {
-        const list = document.createElement("li");
-        list.innerText = user.name;
-        lists.appendChild(list);
-    });
+    users.forEach(addList);
 }
 
 button.addEventListener("click", listUsers);
