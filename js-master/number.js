8aba010bc784418b60ca8ebb18d2ea8c526bfb4a
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-19 13:51:48 +0900

    commit

--- js-master/number.js
+++ js-master/number.js
@@ -2,7 +2,11 @@ const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 
 button.addEventListener("click", async function() {
+    // データのやり取り
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
     console.log(users);
+
+    // DOM操作
+    
 });
