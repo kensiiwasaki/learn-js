49efa22ca3ea3f7cc36a357ea548bbc433f51497
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-19 14:24:04 +0900

    リファクタリング

--- js-master/number.js
+++ js-master/number.js
@@ -1,5 +1,14 @@
+//DOM
 const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
+
+// 関数
+function addList(user) {
+    // DOM操作
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    lists.appendChild(list);
+}
 
 async function getUsers() {
     // データのやり取り
@@ -8,19 +17,11 @@ async function getUsers() {
     return users;
 }
 
-function addList(user) {
-    // DOM操作
-    const list = document.createElement("li");
-    list.innerText = user.name;
-    lists.appendChild(list);
-}
-
 async function listUsers() {
     const users = await getUsers();
     users.forEach(addList);
 }
 
-button.addEventListener("click", listUsers);
-
-
+// イベント
 window.addEventListener("load", listUsers);
+button.addEventListener("click", listUsers);
