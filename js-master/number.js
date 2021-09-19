71ab43e6ec166963fd258eee168e00bed11ddc44
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-19 13:32:51 +0900

    commit

--- js-master/number.js
+++ js-master/number.js
@@ -1,6 +1,7 @@
 async function callApi() {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(res);
+    const users = await res.json();
+    console.log(users);
 }
 
 callApi();
